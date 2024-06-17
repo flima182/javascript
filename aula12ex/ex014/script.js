a053a3d2ae80d8b695df8478171a29fe9ccd5e99
Fernando Lima <141176458+flima182@users.noreply.github.com>
2024-06-17 10:54:35 -0300

    upd_exercicio

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()      
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = '/aula12ex/ex014/image/fotomanha.png'
+        document.body.style.background = '#E2CD9F'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = '/aula12ex/ex014/image/fototarde.png'
+        document.body.style.background = '#B9846F'
+    } else {
+        //BOA NOITE!
+        img.src = '/aula12ex/ex014/image/fotonoite.png'
+        document.body.style.background = '#515154'
+    }
+}
 
